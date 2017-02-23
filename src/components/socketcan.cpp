34fbb4888b2df55a86db9ebf23954681e380ed40
Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>
2017-02-23 15:59:23 +0100

    qt socketcan - first version working
    Signed-off-by: Remigiusz Kołłątaj <remigiusz.kollataj@mobica.com>

--- src/components/socketcan.cpp
+++ src/components/socketcan.cpp
@@ -1,6 +1,2 @@
 #include "socketcan.h"
 
-SocketCan::SocketCan(const std::string &&ifname)
-{
-    mName = ifname;
-}
--- tests/socketcan_test.cpp
+++ tests/socketcan_test.cpp
@@ -2,13 +2,27 @@
 #include <fakeit.hpp>
 #include <iostream>
 #include <components/socketcan.h>
-
-TEST_CASE("SocketCAN", "test") {
-    SocketCan sc("can0");
-}
+#include <QCoreApplication>
+#include <QCanBus>
+#include <QCanBusFrame>
+#include <QCanBusDevice>
+#include <QVariant>
+#include <QDebug>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
-    return Catch::Session().run(argc, argv);
+    QCoreApplication a(argc, argv);
+ 
+    SocketCan sc;
+    Settings s {"socketcan", "can0"};
+
+    sc.configure(s);
+    sc.start();
+    sc.connect(EventType::CanFrame, [] (const CanFrame &cf) {
+                std::cout << cf.id << std::endl;
+            });
+
+    return a.exec();
 }
 
